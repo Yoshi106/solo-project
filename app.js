4f3e6a1403d0ffcaf76e3362f66861730fe0fd14
Yoshi106 <yshkbr@gmail.com>
2020-10-19 15:28:27 +0900

    knex installed grobal

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require("express");
+const knexfile = require("./knexfile");
+const knex = require("knex")(knexfile.development);
 
+knex
+  .select()
+  .table("users")
+  .then((rows) => {
+    console.log(rows);
+  });
+
+const express = require("express");
 
 const app = express();
 
 app.use(express.static("./"));
 
-app.get('/hello', (_, res) =>{
-    res.send("Hello")
+app.get("/hello", (_, res) => {
+  res.send("Hello");
 });
 
 app.listen(5000, () => {
-    console.log("litening @ 5000");
-})
+  console.log("litening @ 5000");
+});
